e2803550cda4e470fb659ea87cf539e2123aa3f6
John Cassidy <jpcassidy@outlook.com>
2020-10-03 13:50:16 -0400

    Improve the UX of the app with the Angular Material Fixes #5

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,8 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { injectSpy } from 'angular-unit-test-helper';
 import { of } from 'rxjs';
 
+import { MaterialModule } from '../material.module';
 import { WeatherService } from '../weather/weather.service';
+import { fakeWeather } from '../weather/weather.service.fake';
 import { CurrentWeatherComponent } from './current-weather.component';
 
 describe('CurrentWeatherComponent', () => {
@@ -16,6 +19,7 @@ describe('CurrentWeatherComponent', () => {
     ]);
     await TestBed.configureTestingModule({
       declarations: [CurrentWeatherComponent],
+      imports: [MaterialModule],
       providers: [{ provide: WeatherService, useValue: weatherServiceSpy }],
     }).compileComponents();
     weatherServiceMock = injectSpy(WeatherService);
@@ -48,20 +52,38 @@ describe('CurrentWeatherComponent', () => {
     expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
   });
 
-  // it('should eagerly load currentWeather in Boston from weatherService', () => {
-  //   // arrange
-  //   weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+  it('should eagerly load currentweather in boston from weatherservice', () => {
+    // arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
 
-  //   // act
-  //   fixture.detectChanges(); // triggers ngOnInit()
+    // act
+    fixture.detectChanges(); // triggers ngoninit()
 
-  //   // assert
-  //   expect(component.current).toBeDefined();
-  //   expect(component.current.city).toEqual('Boston');
+    // assert
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Boston');
+  });
 
-  //   // assert on DOM
-  //   const debugE1 = fixture.debugElement;
-  //   const titleE1: HTMLElement = debugE1.query(By.css('span')).nativeElement;
-  //   expect(titleE1.textContent).toContain('Boston');
-  // });
+  it('should display current weather information correctly', () => {
+    // arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+
+    // act
+    fixture.detectChanges(); // triggers ngoninit()
+
+    // assert
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Boston');
+
+    // assert on dom
+    const debuge1 = fixture.debugElement;
+    const titlee1: HTMLElement = debuge1.query(By.css('.mat-title')).nativeElement;
+    expect(titlee1.textContent).toContain('Boston, US');
+    const datee1: HTMLElement = debuge1.query(By.css('.mat-subheading-2')).nativeElement;
+    expect(datee1.textContent).toContain('Saturday Jan 17th');
+    const tempe1: HTMLElement = debuge1.query(By.css('.mat-display-3')).nativeElement;
+    expect(tempe1.textContent).toContain('280');
+    const desce1: HTMLElement = debuge1.query(By.css('.mat-caption')).nativeElement;
+    expect(desce1.textContent).toContain('light intensity drizzle');
+  });
 });
